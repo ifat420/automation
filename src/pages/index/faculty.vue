9c5a871d5e3513e0723e22b5686967fe3859cf8e
Iftakhar Ahmed <ifat802@gmail.com>
2018-07-22 03:09:27 +0600

    faculty-1

---
+++ src/pages/index/faculty.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6">
+                <h2>Faculty Name</h2>
+            </div>
+            <div class="col-6 text-right">
+                <button class="btn btn-primary">Button </button>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group">
+                    <label for="facultyName">Faculty Name:</label>
+                    <input type="text" class="form-control" id="faculty" name="facultyName" placeholder="Faculty Name">
+                </div>
+            </div>
+            <div class="col-6"></div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        data() {
+            return {
+
+            }
+        }
+    }    
+</script>
