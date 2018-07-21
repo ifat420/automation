53df256108f9391d87950db920c242121463798c
swadhIn <swadhin.just@gmail.com>
2018-07-22 02:58:59 +0600

    second update

--- src/components/home/sidebar.vue
+++ src/components/home/sidebar.vue
@@ -1,18 +1,31 @@
 <template>
     <div class="sidebar">
+        <div class="sidebar__user">
+            <div class="circle sidebar__user--round">
+
+            </div>
+        </div>
         <ul class="sidebar__list">
-            <li class="sidebar__list--item">
-                <p>Academic Info</p>
-                <ul class="sidebar__sub-list">
+            <li class="">  
+                <a data-toggle="collapse" href="#academicInfo" class="sidebar__list--item">
+                    <i class="fas fa-fw fa-university"></i>
+                    Academic Info
+                    <i class="fas fa-angle-down"></i>
+                </a>
+                <ul id="academicInfo" class="sidebar__sub-list collapse">
                     <li class="sidebar__sub-list--item">Teacher</li> 
                     <li class="sidebar__sub-list--item">Student</li> 
                     <li class="sidebar__sub-list--item">Stuff</li> 
                     <li class="sidebar__sub-list--item">Hall</li> 
                 </ul>
             </li>
-            <li class="sidebar__list--item">
-                <p>Users</p>
-                <ul class="sidebar__sub-list">
+            <li class="">
+                <a data-toggle="collapse" href="#users" class="sidebar__list--item">
+                    <i class="fas fa-fw fa-user-shield"></i>
+                    Users
+                    <i class="fas fa-angle-down"></i>
+                </a>
+                <ul id="users" class="sidebar__sub-list collapse">
                     <li class="sidebar__sub-list--item">Super</li> 
                     <li class="sidebar__sub-list--item">Admin</li> 
                     <li class="sidebar__sub-list--item">Teacher</li> 
@@ -20,33 +33,53 @@
                     <li class="sidebar__sub-list--item">Stuff</li> 
                 </ul>
             </li>
-            <li class="sidebar__list--item">
-                <p>Faculty</p>
+            <li class="">
+                <a href="" class="sidebar__list--item">
+                    <i class="fas fa-fw fa-building"></i>
+                    Faculty
+                </a> 
             </li>
-            <li class="sidebar__list--item">
-                <p>Department</p>
+            <li class="">
+                <a href="" class="sidebar__list--item">
+                    <i class="fas fa-fw fa-book"></i>
+                    Department
+                </a> 
             </li>
-            <li class="sidebar__list--item">
-                <p>Program</p>
+            <li class="">
+                <a href="" class="sidebar__list--item">
+                    <i class="fas fa-fw fa-pencil-alt"></i>
+                    Program
+                </a> 
             </li>
-            <li class="sidebar__list--item">
-                <p>Session</p>
-                <ul class="sidebar__sub-list">
+            <li class="">
+                <a data-toggle="collapse" href="#session" class="sidebar__list--item">
+                    <i class="fas fa-fw fa-cogs"></i>
+                    Session
+                    <i class="fas fa-angle-down"></i>
+                </a>
+                <ul id="session" class="sidebar__sub-list collapse">
                     <li class="sidebar__sub-list--item">Add New</li> 
                     <li class="sidebar__sub-list--item">Catalog</li>  
                 </ul>
             </li>
-            <li class="sidebar__list--item">
-                <p>Course</p>
-                <ul class="sidebar__sub-list">
+            <li class="">
+                <a data-toggle="collapse" href="#course" class="sidebar__list--item">
+                    <i class="fas fa-fw fa-tasks"></i>
+                    Course
+                    <i class="fas fa-angle-down"></i>
+                </a>
+                <ul id="course" class="sidebar__sub-list collapse">
                     <li class="sidebar__sub-list--item">Add New</li> 
                     <li class="sidebar__sub-list--item">Catalog</li> 
                     <li class="sidebar__sub-list--item">Assign</li> 
                     <li class="sidebar__sub-list--item">Registration</li>  
                 </ul>
             </li>
-            <li class="sidebar__list--item">
-                <p>Marks</p>
+            <li class=""> 
+                <a href="" class="sidebar__list--item">
+                    <i class="fas fa-fw fa-edit"></i>
+                    Marks
+                </a> 
             </li>
         </ul>
     </div>
@@ -54,7 +87,40 @@
 
 <script>
 export default {
-    
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        // reveal(event) {   
+        //     return;
+        //     var tg = event.target.tagName;
+        //     var el = event.target;
+        //     var trgt;
+
+        //     if(tg == 'P') el = event.target.parentElement;  
+            
+        //     for(let i of el.childNodes) {
+        //         if(i.tagName == 'UL') {
+        //             trgt = i;
+        //             break;
+        //         } 
+        //     }
+
+        //     if(trgt && trgt.classList.contains('d-none')) trgt.classList.remove('d-none')
+        //     else if(trgt) trgt.classList.add('d-none')
+        // }
+    },
+    mounted() { 
+        this.$nextTick().then(function(){
+            // $("#sidebar").mCustomScrollbar({
+            //     theme: "minimal"
+            // });
+        })
+    }
 }
 </script>
 
+<style scoped> 
+</style>
--- src/main.js
+++ src/main.js
@@ -10,7 +10,54 @@ global.jQuery = jQuery
 global.Popper = popper
 
 import 'bootstrap' //js
- 
+
+// font awesome
+import fontawesome from '@fortawesome/fontawesome'
+import faUniversity from '@fortawesome/fontawesome-free-solid/faUniversity'
+import faUserShield from '@fortawesome/fontawesome-free-solid/faUserShield'
+import faPencilAlt from '@fortawesome/fontawesome-free-solid/faPencilAlt'
+import faEdit from '@fortawesome/fontawesome-free-solid/faEdit'
+import faTasks from '@fortawesome/fontawesome-free-solid/faTasks'
+import faCogs from '@fortawesome/fontawesome-free-solid/faCogs'
+import faBuilding from '@fortawesome/fontawesome-free-solid/faBuilding'
+import faBook from '@fortawesome/fontawesome-free-solid/faBook'
+import faAngleDown from '@fortawesome/fontawesome-free-solid/faAngleDown'
+import faExclamationTriangle from '@fortawesome/fontawesome-free-solid/faExclamationTriangle'
+
+import faFacebookF from '@fortawesome/fontawesome-free-brands/faFacebookF' 
+import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebook' 
+import faSkype from '@fortawesome/fontawesome-free-brands/faSkype' 
+import faYoutube from '@fortawesome/fontawesome-free-brands/faYoutube' 
+import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter' 
+import faVimeo from '@fortawesome/fontawesome-free-brands/faVimeo' 
+import faPinterest from '@fortawesome/fontawesome-free-brands/faPinterest' 
+import faPinterestP from '@fortawesome/fontawesome-free-brands/faPinterestP' 
+import faGooglePlus from '@fortawesome/fontawesome-free-brands/faGooglePlus' 
+import faGooglePlusG from '@fortawesome/fontawesome-free-brands/faGooglePlusG' 
+
+fontawesome.library.add(faUniversity)
+fontawesome.library.add(faUserShield)
+fontawesome.library.add(faPencilAlt)
+fontawesome.library.add(faEdit)
+fontawesome.library.add(faTasks)
+fontawesome.library.add(faCogs)
+fontawesome.library.add(faBuilding)
+fontawesome.library.add(faBook)
+fontawesome.library.add(faAngleDown)
+fontawesome.library.add(faExclamationTriangle)
+
+
+fontawesome.library.add(faFacebookF) 
+fontawesome.library.add(faFacebook) 
+fontawesome.library.add(faSkype) 
+fontawesome.library.add(faYoutube) 
+fontawesome.library.add(faTwitter) 
+fontawesome.library.add(faVimeo) 
+fontawesome.library.add(faPinterest) 
+fontawesome.library.add(faPinterestP) 
+fontawesome.library.add(faGooglePlus) 
+fontawesome.library.add(faGooglePlusG) 
+//end font awesome
 
 Vue.config.productionTip = false
 
